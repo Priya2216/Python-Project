58c459d7709a42eec5bd1304851c9b1e90bfe4b6
priya2216 <priyaraut77492@gmail.com>
2022-03-16 10:24:54 +0530

    first file

---
+++ New folder/main.py
@@ -0,0 +1,12 @@
+from superbike import SuperBike
+
+bmw = SuperBike()
+
+bmw.start()
+
+bmw.accelerate()
+
+bmw.brake()
+
+# in method overriding super bike over rides the start() function on bike
+# function
